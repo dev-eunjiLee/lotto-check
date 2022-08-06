2fa8d2b8d9849415f2edaf0b367118775c80e0aa
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-07 01:02:12 +0900

    [ADD]: (main) css 작업중

--- App.tsx
+++ App.tsx
@@ -3,18 +3,44 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles_layout.container}>
+      <View style={styles_layout.header}>
+        <Text style={styles_text.header_title}>오늘의 로또</Text>
+      </View>
+      <View style={styles_layout.body}>
+        <Text style={styles_text.body_content}>오늘 구매한 로또 번호 등록</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const styles_layout = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fcba03',
+    },
+    header: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    body: {
+      flex: 1,
+      fontSize: 50,
+      fontWeight:"bold",
+      alignItems: 'center',
+      alignContent: "center"
+    }
+});
+
+const styles_text = StyleSheet.create({
+  header_title: {
+    fontSize: 50,
+    fontWeight:"bold",
+    alignContent: "center"
   },
+  body_content: {
+    fontSize: 20,
+    alignContent: "center"
+  }
 });
