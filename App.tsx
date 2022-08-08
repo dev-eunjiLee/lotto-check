9a62d218fa3eee12b4dc4e3a49b1dfbc68fd150e
Eunji Lee <74188470+dev-eunjiLee@users.noreply.github.com>
2022-08-08 23:58:18 +0900

    FIX: (APP) 주석 추가, 유저 위치를 기반으로 복권 판매점 데이터 가져오기

--- App.tsx
+++ App.tsx
@@ -12,15 +12,19 @@ export class LocationType {
 export default function App() {
   const [locationPermission, setLocationPermission] = useState<boolean>(false);
   const [userLocation, setUserLocation] = useState<LocationType>();
+  // * ===== 기타 함수 ===== * //
+  // * 위치 정보 사용해도 되는지 물어보기
   const ask = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     setLocationPermission(granted);
   };
+  // * 위치 정보 가져오기
   const getUserLocation = async (locationPermission: boolean) => {
     if (locationPermission) {
       const {
         coords: { longitude, latitude },
       } = await Location.getCurrentPositionAsync();
+      console.log(longitude, latitude);
       const [{ city, district }] = await Location.reverseGeocodeAsync({
         longitude,
         latitude,
@@ -33,6 +37,38 @@ export default function App() {
       }
     }
   };
+  // * 주소를 기반으로 로또 판매점 가져오기
+  const getLottoStoreList = async (userLocation?: LocationType) => {
+    console.log(userLocation);
+    const API_KEY = "09062df488493f65510e46700fa97428";
+
+    try {
+      const formData = new FormData();
+      formData.append("searchType", "2");
+      formData.append("kind", "1");
+      formData.append("srchval", "가좌동");
+      // fetch 참고 링크: https://stackoverflow.com/questions/47438466/react-native-fetch-returns-error-json-unexpected-eof
+      const response = fetch(
+        "https://dhlottery.co.kr/store.do?method=sellerInfo645Result",
+        {
+          method: "POST",
+          headers: {
+            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+          },
+
+          body: formData,
+          mode: "cors",
+          credentials: "include",
+        }
+      ).then((res) => {
+        console.log(JSON.stringify(res, null, 4));
+        return res.json();
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // * ===== useEffect ===== * //
   // 권한 요청 => useEffect의 2번째 파라미터를 빈 배열로 하면 처음 렌더링 될 때 사용된다
   useEffect(() => {
@@ -47,6 +83,12 @@ export default function App() {
       await getUserLocation(locationPermission);
     })();
   }, [locationPermission]);
+  // 유저 위치를 기반으로 복권 판매점 데이터 가져오기
+  useEffect(() => {
+    (async () => {
+      await getLottoStoreList(userLocation);
+    })();
+  }, [userLocation]);
 
   // * ===== return ===== * //
   return (
